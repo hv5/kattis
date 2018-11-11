f362c431806f91dd1568c891511596c1daa82168
hv5 <per.evje@gmail.com>
2018-11-11 23:43:56 +0100

    Add Rimski project

---
+++ Rimski/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rimski
+{
+   class Program
+    {
+        private string _str;
+
+        void Run()
+        {
+            _str = Console.ReadLine();
+
+            Process();    
+        }
+
+        void Process()
+        {
+            Console.Error.WriteLine(_str);
+        }
+
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.Run();
+        }
+    }
+}
---
+++ Rimski/README.md
@@ -0,0 +1,11 @@
+# Rimski
+
+### [Problem description](https://open.kattis.com/problems/rimski)
+
+### Build
+- dotnet build
+
+### Run
+- type data\rimski.dummy.1.in | dotnet run
+- type data\rimski.dummy.2.in | dotnet run
+- type data\rimski.dummy.3.in | dotnet run
