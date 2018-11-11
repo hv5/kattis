323a2bf349e86b467355ac2c779810f17988f722
hv5 <per.evje@gmail.com>
2018-11-11 22:53:07 +0100

    ParsingHex - add logic

--- ParsingHex/Program.cs
+++ ParsingHex/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ParsingHex
 {
    class Program
     {  
-        void Run()
+        private string _str;
+        private Regex _regex;
+
+        public Program()
         {
-            string line;
-            while ((line = Console.ReadLine()) != null)
-                Process(line);     
+            _regex = new Regex(@"(0[xX][0-9a-fA-F]+)"); // hexadecimal 
         }
 
-        void Process(string str)
+        void Run()
         {
-            Console.Error.WriteLine(str);
+            while ((_str = Console.ReadLine()) != null) 
+                Process();     
+        }
 
+        void Process()
+        {
+            //Console.Error.WriteLine(_str);
 
+            foreach (Match match in _regex.Matches(_str))
+            {
+                string hexValue = match.Value;
+                int decValue = Convert.ToInt32(hexValue, 16);
+                Console.Error.WriteLine("{0} {1}", hexValue, decValue);
+            }          
         }
 
         static void Main(string[] args)
