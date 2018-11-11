f7c3e3df711e2abccfbb3777f1902c0d584029f3
hv5 <per.evje@gmail.com>
2018-11-11 19:29:34 +0100

    Add SimpleAddition project

---
+++ SimpleAddition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+namespace SimpleAddition
+{
+   class Program
+    {
+        private string _str1;
+        private string _str2;
+
+
+        void Run()
+        {    
+            _str1 = Console.ReadLine();
+            _str2 = Console.ReadLine();
+
+            Process();
+        }
+
+        void Process()
+        {
+            //Console.Error.WriteLine(_str1);
+            //Console.Error.WriteLine(_str2); 
+
+            BigInteger bigInt1 = BigInteger.Parse(_str1);
+            BigInteger bigInt2 = BigInteger.Parse(_str2);
+
+            BigInteger bigIntSum = bigInt1 + bigInt2;
+
+            Console.WriteLine(bigIntSum.ToString());
+            
+        }
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.Run();
+        }
+    }
+}
---
+++ SimpleAddition/README.md
@@ -0,0 +1,10 @@
+# Simple Addition
+
+### [Problem description](https://open.kattis.com/problems/simpleaddition)
+
+### Build
+- dotnet build
+
+### Run
+- type data\1.in | dotnet run
+- type data\2.in | dotnet run
